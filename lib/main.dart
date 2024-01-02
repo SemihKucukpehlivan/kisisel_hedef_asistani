44fe32c50bc1094631489f03c8d04ac24d3c24c6
Semih Küçükpehlivan <94116102+SemihKucukpehlivan@users.noreply.github.com>
2024-01-02 22:13:15 +0300

    Cloud Messageing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kisisel_hedef_asistani/api/firebaseApi.dart';
 import 'package:kisisel_hedef_asistani/firebase_options.dart';
 import 'package:kisisel_hedef_asistani/screens/loginAndSignUpScreen/loginScreen.dart';
 
@@ -8,6 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
